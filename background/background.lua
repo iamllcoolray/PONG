0ed9e5bcd33f21fd930032b545b6b61e894a5f7f
iamllcoolray <llcoolray24@gmail.com>
2020-12-30 00:33:16 -0500

    added background audio

--- background/background.lua
+++ background/background.lua
@@ -8,10 +8,16 @@ function Background:load()
   self.width = self.planets:getWidth()
   self.height = self.planets:getHeight()
   self.rotation = 0
+  self.alien_space = love.audio.newSource("audio/alien-space.mp3", "stream")
 end
 
 function Background:update(dt)
   self.rotation = self.rotation + 0.05 * dt
+
+  self.alien_space:setLooping(true)
+  self.alien_space:setPitch(0.5)
+  self.alien_space:setVolume(0.5)
+  self.alien_space:play()
 end
 
 function Background:draw()
