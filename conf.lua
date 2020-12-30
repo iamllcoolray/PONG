d7032080597a66e65852b1f9c06154e6111ed085
iamllcoolray <llcoolray24@gmail.com>
2020-12-29 20:08:29 -0500

    initial commit

---
+++ conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+  t.window.title = "Pong"
+  t.version = "11.3"
+  t.window.width = 1200
+  t.window.height = 600
+end
---
+++ main.lua
@@ -0,0 +1,44 @@
+require("background")
+require("player")
+require("ball")
+require("ai")
+
+function love.load()
+  Background:load()
+  Player:load()
+  Ball:load()
+  AI:load()
+
+  Score = {player = 0, ai = 0}
+  font = love.graphics.newFont("assets/audiowide.ttf", 30)
+end
+
+function love.update(dt)
+  Background:update(dt)
+  Player:update(dt)
+  Ball:update(dt)
+  AI:update(dt)
+end
+
+function love.draw()
+  Background:draw()
+  Player:draw()
+  Ball:draw()
+  AI:draw()
+
+  drawScore()
+end
+
+function drawScore()
+  love.graphics.setFont(font)
+  love.graphics.print("Player: "..Score.player, 50, 50)
+  love.graphics.print("AI: "..Score.ai, 1000, 50)
+end
+
+function checkCollision(a, b)
+  if a.x + a.width > b.x and a.x < b.x + b. width and a.y + a.height > b.y and a.y < b.y + b.height then
+    return true
+  else
+    return false
+  end
+end
